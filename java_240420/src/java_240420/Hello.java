cc2a15e45f1e38382103cca845c813fc9298f0ae
carlton777 <sangkoo@naver.com>
2024-04-20 10:27:00 +0900

    주석달기 & 파일 추가

--- java_240420/src/java_240420/Hello.java
+++ java_240420/src/java_240420/Hello.java
@@ -1,5 +1,8 @@
 package java_240420;
 
+//test
+//fdsaf
+//4324
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello~");
---
+++ java_240420/src/java_240420/Home.java
@@ -0,0 +1,7 @@
+package java_240420;
+
+public class Home {
+	public static void main(String[] args) {
+		System.out.println("home~");
+	}
+}
