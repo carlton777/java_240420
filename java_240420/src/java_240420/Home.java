db8c0971158a73b84acf24cfbb63481a4799233c
carlton777 <sangkoo@naver.com>
2024-04-20 11:57:05 +0900

    777 888

--- java_240420/src/java_240420/Home.java
+++ java_240420/src/java_240420/Home.java
@@ -2,6 +2,7 @@ package java_240420;
 
 public class Home {
 	public static void main(String[] args) {
-		System.out.println("home~");
+		System.out.println("home777~");
+		System.out.println("home888~");
 	}
 }
